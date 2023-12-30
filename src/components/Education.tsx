49db579c49832dcf5948106d4bb42a2f5b1c32e8
Addy <patiladiti752@gmail.com>
2023-12-29 17:31:28 -0800

    Edu section changes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -3,17 +3,30 @@ import EducationItem from "./EducationItem";
 const educationData = [
     // Add your education data here in a similar format to the work experience data
     {
-        year: "2023-2025",
+        year: "2023 - 2025",
         institution: "San Jose State University",
             degree: "Master of Science in Software Engineering",
         description: "GPA: 3.9/4.0"
     },
     {
-        year: "2017-2021",
+        year: "2017 - 2021",
         institution: "Cummins College Of Engineering for Women, Pune ",
         degree: "Bachelor of Technology in Information Technology",
         description: "GPA: 8.8/10.0"
+    },
+    {
+        year: "2015 - 2017",
+        institution: "Deogiri College, Aurangabad",
+        degree: "XII HSC (Science)",
+        description: "Score: 92%"
+    },
+    {
+        year: "2008 - 2015",
+        institution: "St. Lawrence High School, Aurangabad",
+        degree: "X SSC",
+        description: "Score: 95%"
     }
+    
 ];
 
 const Education = () => {
